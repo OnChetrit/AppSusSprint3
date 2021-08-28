49cb810c5455518cd0c6176139806fb4fa3217d4
Adir cohen <adirco123@gmail.com>
2021-08-28 18:35:53 +0300

    hljkh;p

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -7,7 +7,7 @@ class _AppHeader extends React.Component {
     const logo = this.props.match.path === '/user/:userId/mail' ? './img/our-logo.png' : this.props.match.path === '/user/:userId/keep' ? './img/keep-react.png' : this.props.match.path === '/book' ?'./img/book-react.png' : ''
     return (
       <nav className="app-header flex al-items-center">
-        <img src={logo}/>
+        <img src={logo} />
         <NavLink to="/">LogOut</NavLink>
         <NavLink to={`/book`}>Book</NavLink>
         <NavLink to={`/user/${user.id}/keep`}>Keep</NavLink>
